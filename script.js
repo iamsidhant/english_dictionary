4df80bebcf0874d3ae1cacc9e39ae7cb14b3862a
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-12-31 23:28:10 +0530

    dictionary

--- script.js
+++ script.js
@@ -5,18 +5,36 @@ const titleEl = document.getElementById("title");
 const meaningEl = document.getElementById("meaning");
 const audioEl = document.getElementById("audio");
 
-async function fetchAPI(){
+async function fetchAPI(word){
     try {
         infoTextEl.style.display = "block";
         meaningContainerEl.style.display = "none";
         infoTextEl.innerText = `Searching the meaning of "${word}"`;
+        const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+        const result = await fetch(url).then((res) => res.json());
+
+        if (result.title) {
+            meaningContainerEl.style.display = "none";
+            infoTextEl.style.display = "block";
+            titleEl.innerText = word;
+            meaningEl.innerText = "N/A";
+            audioEl.style.display = "none";
+
+        } else {
+            infoTextEl.style.display = "block";
+            meaningContainerEl.style.display = "none";
+            audioEl.style.display = "inline-flex";
+            titleEl.innerText = result[0].word;
+            meaningEl.innerText = result[0].meanings[0].definitions[0].definition;
+            audioEl.src = result[0].phonetics[0].audio;
+        }
     } catch (error) {
         console.log(error);
         infoTextEl.innerText = `An error happened, try again later`
     }
 }
 
-inputEl.addEventListener("key", (e) => {
+inputEl.addEventListener("keyup", (e) => {
     if (e.target.value && e.key === "Enter") {
         fetchAPI(e.target.value);
     }
